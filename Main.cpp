0af0403884b69fd91dc8b69455b7b1a58a71434b
Earl <ex3mpli@users.noreply.github.com>
2016-09-14 22:02:30 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -17,7 +17,7 @@ void ex3mpli()
   while(1) {
   dwSize = 0x500000; 
   
-  DWORD aIntersectSegment       = FindPattern((DWORD)GetModuleHandle("Crossfire.exe"),0xFFFFFF,(PBYTE)"\x5D\xC3\xCC\x55\x8B\xEC\x8B\x45\x0C\x50\x8B\x4D\x08\x51\x8B\x15\x00\x00\x00\x00","xxxxxxxxxxxxxxxx????");
+  DWORD aIntersectSegment       = FindPattern((DWORD)GetModuleHandleA("crossfire.exe"),0xFFFFFF,(PBYTE)"\x5D\xC3\xCC\x55\x8B\xEC\x8B\x45\x0C\x50\x8B\x4D\x08\x51\x8B\x15\x00\x00\x00\x00","xxxxxxxxxxxxxxxx????");
   DWORD NoReload                = FindPattern((DWORD)GetModuleHandleA("CShell.dll"),0xFFFFFF,(PBYTE)"\x75\x21\x80\x7B\x00\x00","xxxx??");
   DWORD NoFallDamage            = FindPattern((DWORD)GetModuleHandleA("CShell.dll"),0xFFFFFF,(PBYTE)"\x75\x05\xBD\x00\x00\x00\x00\x8B\x17","xxx????xx");
   DWORD WeaponMgr               = FindPattern((DWORD)GetModuleHandleA("CShell.dll"),0xFFFFFF,(PBYTE)"\x8B\x0D\x00\x00\x00\x00\x8B\x04\xB1\xD9\xE8","xx????xxxxx");
@@ -56,7 +56,7 @@ extern "C" __declspec(dllexport) BOOL WINAPI DllMain ( HMODULE hDll, DWORD dwRea
   CreateThread(0,0,(LPTHREAD_START_ROUTINE)ex3mpli,0,0,0);
 }
   if(dwReason==DLL_PROCESS_DETACH) {
-    file.close;
+    exit(0);
   }
   return TRUE;
 }
