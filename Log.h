41b856a1dd56fa96b3eb35f988b3fbb712d8ada0
Earl <ex3mpli@users.noreply.github.com>
2016-09-14 19:04:55 +0800

    Update Log.h

--- Log.h
+++ Log.h
@@ -1,4 +1,3 @@
 char *GetDirectoryFile(char *filename);
 void __cdecl Writelog (const char * fmt, ...);
 void logging(HMODULE hDll);
-#pragma message("master.h : Building Addylogger. Credits:Dean-Wingess, Neo I.I.I")
