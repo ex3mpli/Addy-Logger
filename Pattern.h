f17814dce9906dac1cfd5190f6b61dac452c9802
Earl <ex3mpli@users.noreply.github.com>
2016-09-14 19:02:45 +0800

    Update Pattern.h

--- Pattern.h
+++ Pattern.h
@@ -1,21 +1,21 @@
 DWORD zSize;
 DWORD zStartAddress;
 
-BOOL bCompare(const BYTE* zData, const BYTE* zMask, const char* zzMask)
+BOOL bCompare(const BYTE* zData, const BYTE* bMask, const char* zzMask)
 {
-for(;*zzMask;++zzMask,++zData,++zMask)
+for(;*zzMask;++zzMask,++zData,++bMask)
 {
-if(*zzMask == 'x' && *zData != *zMask)
+if(*zzMask == 'x' && *zData != *bMask)
 return false;
 }
 return (*zzMask)==NULL;
 }
 
 
-DWORD FindPattern(DWORD zAddress,DWORD zLen,BYTE *zMask,char * zzMask)
+DWORD FindPattern(DWORD zAddress,DWORD zLen,BYTE *bMask,char * zzMask)
 {
 for(DWORD j = 0; j < zLen; j++)
-if( bCompare((BYTE*)(dwAddress+j),zMask,zzMask))
+if( bCompare((BYTE*)(dwAddress+j),bMask,zzMask))
 return (DWORD)(zAddress+j);
 return false;
 }
