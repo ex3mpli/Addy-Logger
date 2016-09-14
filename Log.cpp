6be545136a7ba1b2e6f968bd7d878ced2dea978d
Earl <ex3mpli@users.noreply.github.com>
2016-09-14 22:39:15 +0800

    Update Log.cpp

--- Log.cpp
+++ Log.cpp
@@ -35,5 +35,5 @@ void logging(HMODULE hDll){
 DisableThreadLibraryCalls(hDll);
 GetModuleFileNameA(hDll, dlldirectory, 512);
 for(int i = strlen(dlldirectory); i > 0; i--) { if(dlldirectory[i] == '\\') { dlldirectory[i+1] = 0; break; } }
-ofile.open(GetDirectoryFile("Adresses.txt"), ios::app); // here put the output filename
+ofile.open(GetDirectoryFile("ex3mpli Addy Logger.txt"), ios::app);
 }
