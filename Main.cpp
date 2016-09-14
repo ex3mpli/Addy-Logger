2859e6062ac561f9d92a9395fca2c4aa148a3fd2
Earl <ex3mpli@users.noreply.github.com>
2016-09-13 22:22:56 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,55 +3,49 @@
 #include "Log.h"
 #include "Pattern.h"
 
-bool IsGameReadyForHook()
-{
-if( GetModuleHandleA( "ClientFX.fxd" ) != NULL 
-&& GetModuleHandleA( "CShell.dll" ) != NULL )
-return true;
-
-return false;
+bool ex3mpliReady() {
+  if (GetModuleHandleA( "ClientFx.fxd" ) != NULL && GetModuleHandleA( "CShell.dll" ) != NULL)
+    return true;
+  return false;
 }
 
-void SearchPatterns(void)
+void ex3mpli()
 {
-while (!IsGameReadyForHook()){
-Sleep(50);
+  while (!ex3mpliReady()) {
+  Sleep(100);
 }
-while(true){
-dwSize = 0x500000; 
+  while(1) {
+  dwSize = 0x400000; 
 
 //Example DWORD dwPlayerPointer = FindPattern((PBYTE)"\x00\x00\x00\x00\x00\x00\x00};", "x????xxxx", 1, true);
 
-DWORD nametags1 = FindPattern((DWORD)GetModuleHandleA("CShell.dll"), 0x9c0000, (PBYTE)"\x75\x21\x80\x7B\x00\x00", "xxxx??");
-DWORD nametags2 = FindPattern((DWORD)GetModuleHandleA("CShell.dll"), 0x9c0000, (PBYTE)"\x75\x05\xBD\x00\x00\x00\x00\x8B\x17", "xxx????xx");
+  DWORD nametags1 = FindPattern((DWORD)GetModuleHandleA("CShell.dll"), 0x9c0000, (PBYTE)"\x75\x21\x80\x7B\x00\x00", "xxxx??");
+  DWORD nametags2 = FindPattern((DWORD)GetModuleHandleA("CShell.dll"), 0x9c0000, (PBYTE)"\x75\x05\xBD\x00\x00\x00\x00\x8B\x17", "xxx????xx");
 //-------------------------------------------------------------//
-Writelog("//==============Dean-Wingess N3x0n Logger==============\\");
-Writelog("//==============Created and developed by:==============\\");
-Writelog("//======================Dean-Wingess===================\\");
-Writelog("//====================== Neo I.I.I ===================\\");
-Writelog("//====================== FlaVour ===================\\");
-Writelog("");
-Writelog("#define NoReload 0x%X",NoReload);
-Writelog("#define FallDamage 0x%X",FallDamage);
+  Writelog("//=================== ex3mpli Logger ===================\\");
+  Writelog("//============== Created and developed by: ==============\\");
+  Writelog("//====================== ex3mpli ===================\\");
+  Writelog("//====================== Crossfire ===================\\");
+  Writelog("//====================== Philippines ===================\\");
+  Writelog("");
+  Writelog("#define NoReload 0x%X",NoReload);
+  Writelog("#define FallDamage 0x%X",FallDamage);
 
 
-Writelog("");
-Writelog("//*************************Next log*********************************");
+  Writelog("");
+  Writelog("//*************************Next log*********************************");
 
 
 
-ExitProcess(0);
+  ExitProcess(false);
+  }
 }
-}
 
-BOOL WINAPI DllMain ( HMODULE hDll, DWORD dwReason, LPVOID lpReserved )
-{
-DisableThreadLibraryCalls(hDll);
-if (dwReason==DLL_PROCESS_ATTACH)
-{
-logging(hDll);
-ShellExecuteA(0,"open","http://mpgh.net",0,0,SW_MINIMIZE);
-CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SearchPatterns, NULL, NULL, NULL);
+extern "C" __declspec(dllexport) BOOL WINAPI DllMain ( HMODULE hDll, DWORD dwReason, LPVOID lpReserved ) {
+  DisableThreadLibraryCalls(hDll);
+  if (dwReason==DLL_PROCESS_ATTACH) {
+  logging(hDll);
+  CreateThread(0,0,(LPTHREAD_START_ROUTINE)SearchPatterns,0,0,0);
 }
-return TRUE;
+  return TRUE;
 }
