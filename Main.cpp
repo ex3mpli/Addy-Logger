0f31bd5d1cc8892da2e3640348782212640d8548
Earl <ex3mpli@users.noreply.github.com>
2016-09-15 22:16:16 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -19,31 +19,33 @@ void ex3mpli()
   DWORD CShell      = (DWORD)GetModuleHandleA("CShell.dll");
   DWORD Crossfire   = (DWORD)GetModuleHandleA("crossfire.exe");
   
-  DWORD WeaponMgr = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x8B\x0D\x00\x00\x00\x00\x8B\x04\xB1\xD9\xE8","xx????xxxxx");
-  WeaponMgr = *(DWORD*)(WeaponMgr + 0x2) - CShell;
- 
-  DWORD WallArray = FindPattern(0x400000,0xFFFFFF,(PBYTE)"\x75\x00\x83\x0D\x00\x00\x00\x00\x01\xB8\x00\x00\x00\x00\xE8","x?xx????xx????x");
-  WallArray = *(DWORD*)(WallArray + 0xA) - Crossfire;
- 
-  DWORD PlayerMgr = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x8B\x15\x00\x00\x00\x00\xD9\x80\x00\x00\x00\x00\xD9\x9C\x16\x00\x00\x00\x00\xD9\x80\x00\x00\x00\x00\xA1\x00\x00\x00\x00","xx????xx????xxx????xx????x????");
-  PlayerMgr = *(DWORD *)(PlayerMgr + 0x03) - CShell;
-
-  DWORD DamageZone  = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x68\x00\x00\x00\x00\x8D\x4C\x24\x24\x51\x8D\x54\x24\x34\x52","x????xxxxxxxxxx");
+  DWORD WeaponMgr         = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x8B\x0D\x00\x00\x00\x00\x8B\x04\xB1\xD9\xE8","xx????xxxxx");
+  DWORD PlayerBasicInfo   = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x8B\x0D\x00\x00\x00\x00\x83\xC4\x04\x69\xC0\x00\x00\x00\x00", "xx????xxxxx????");
+  DWORD WallArray         = FindPattern(0x400000,0xFFFFFF,(PBYTE)"\x75\x00\x83\x0D\x00\x00\x00\x00\x01\xB8\x00\x00\x00\x00\xE8","x?xx????xx????x");
+  DWORD DamageZone        = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x68\x00\x00\x00\x00\x8D\x4C\x24\x24\x51\x8D\x54\x24\x34\x52","x????xxxxxxxxxx");
+  DWORD ModelNode         = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x8B\x0D\x00\x00\x00\x00\x83\xC4\x04\x89\x44\x0F\x54\x8B\x15\x00\x00\x00\x00\x8B\x04\x17\x3B\xC3\x7C\x0C\x83\xF8\x64\x7D\x07","xx????xxxxxxxxx????xxxxxxxxxxxx");
+  DWORD NoReload          = FindPattern(CShell,0xFFFFFF,(PBYTE)"\xD9\x98\x00\x00\x00\x00\x55\xE8\x00\x00\x00\x00", "xx????xx????")
+  DWORD FastChange        = FindPattern(CShell,0xFFFFFF,(PBYTE)"\xD9\x9A\x00\x00\x00\x00\x55\xE8\x00\x00\x00\x00","xx????xx????");
+  DWORD FastKnife1        = FindPattern(CShell,0xFFFFFF,(PBYTE)"\xD9\x9C\xB9\x00\x00\x00\x00\x83\xC7\x01\x83\xC4\x04\x3B\x7D\x00\x72\xA6","xxx????xxxxxxxxxxx");
+  DWORD FastKnife2        = FindPattern(CShell,0xFFFFFF,(PBYTE)"\xD9\x9C\xB8\x00\x00\x00\x00\x8B\x4C\x24\x14","xxx????xxxx");
+  DWORD AmmoDamge         = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x68\x00\x00\x00\x00\x8B\xCE\xE8\x00\x00\x00\x00\xD9\x83\x00\x00\x00\x00\x51\x8B\xCE\xD9\x1C\x24\xE8\x00\x00\x00\x00\x8B\xCE\xE8\x00\x00\x00\x00\x8B\xCE\xE8\x00\x00\x00\x00\x6A\x00","x????xxx????xx????xxxxxxx????xxx????xxx????xx");
   
-  DWORD ModelNode = FindPattern(CShell,0xFFFFFF,(PBYTE)"\x8B\x0D\x00\x00\x00\x00\x83\xC4\x04\x89\x44\x0F\x54\x8B\x15\x00\x00\x00\x00\x8B\x04\x17\x3B\xC3\x7C\x0C\x83\xF8\x64\x7D\x07","00????000000000????000000000000");
-  ModelNode = *(DWORD*)(ModelNode + 0x2) - CShell;
-  
-  Writelog("----------------| Crossfire Philippines |----------------");
-  Writelog("-----------------| ex3mpli Addy Logger |-----------------");
-  Writelog("-----------------------| CShell |---------------------");
-  Writelog("#define DamageZone \t\t0x00%X",DamageZone);
-  Writelog("#define PlayerMgr \t\t0x00%X",PlayerMgr);
-  Writelog("#define WeaponMgr \t\t0x00%X",WeaponMgr);
-  Writelog("#define ModelNode \t\t0x00%X" ModelNode);
+  Writelog("_______________________________________________");
+  Writelog("|           Crossfire Philippines             |");
+  Writelog("|              ex3mpli Logger                 |");
+  Writelog("_______________________________________________");
   Writelog("");
-  Writelog("-----------------------| Crossfire |---------------------");
-  Writelog("#define WallArray \t\t0x00%X",WallArray+0xA4);
-  Writelog("------------------| ex3mpli Logger END! |----------------");
+  Writelog("#define WallArray \t0x00%X",WallArray);
+  Writelog("");
+  Writelog("#define NoReload \t0x00%X",NoReload);
+  Writelog("#define DamageZone \t0x00%X",DamageZone);
+  Writelog("#define WeaponMgr \t0x00%X",WeaponMgr);
+  Writelog("#define ModelNode \t0x00%X" ModelNode);
+  Writelog("#define PlayerBasicInfo \t0x00%X",PlayerBasicInfo);
+  Writelog("#deinfe FastChange \t0x00%X",FastChange);
+  Writelog("#define FastKnife1 \t0x00%X",FastKnife1);
+  Writelog("#define FastKnife2 \t0x00%X",FastKnife2);
+  Writelog("#define AmmoDamge \t0x00%X",AmmoDamage);
   ExitProcess(false);
   }
 }
