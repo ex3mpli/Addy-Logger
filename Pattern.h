c382f077dbe6a6cd0c40572d532847cf42a2a6f9
Earl <ex3mpli@users.noreply.github.com>
2016-09-22 18:25:24 +0800

    Update Pattern.h

--- Pattern.h
+++ Pattern.h
@@ -3,12 +3,12 @@ DWORD dwSize;
 bool bCompare (const BYTE* pData, const BYTE* bMask, const char* szMask) {
   for(; *szMask; ++szMask, ++pData, ++bMask)
   if(*szMask=='0' && *pData != *bMask)
-  return false;
+  return 0;
   return (*szMask) == 0;
   }
 DWORD FindPattern (DWORD dwAddress,DWORD dwLen,BYTE *bMask,char * szMask) {
   for(DWORD i=0; i<dwLen; i++)
   if (bCompare((BYTE*)(dwAddress + i), bMask, szMask))
   return (DWORD)(dwAddress + i);
-  return false;
+  return 0;
   }
