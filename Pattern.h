047bd07208285cf1d114efaa2b94ad2667c23c6d
Earl <ex3mpli@users.noreply.github.com>
2016-10-06 19:56:06 +0800

    Update Pattern.h

--- Pattern.h
+++ Pattern.h
@@ -1,5 +1,4 @@
 DWORD dwSize;
-DWORD dwStartAddress;
 
 bool bCompare2(const BYTE* pData, const BYTE* bMask, const char* szMask)
 {
@@ -8,27 +7,11 @@ bool bCompare2(const BYTE* pData, const BYTE* bMask, const char* szMask)
   return false;
   return (*szMask) == 0;
 }
-/*************************************************************************
+
 DWORD FindPattern2(DWORD dwAddress,DWORD dwLen,BYTE *bMask,char * szMask)
 {
   for(DWORD i = 0; i < dwLen; i++)
   if (bCompare2((BYTE*)(dwAddress + i), bMask, szMask))
   return (DWORD)(dwAddress + i);
   return false;
-}
-*************************************************************************/
-
-DWORD FindPattern2(BYTE *bMask, char * szMask, int codeOffset, BOOL extract)
-{
-  for(DWORD i = 0; i < dwSize; i++) {
-    if(bCompare2((BYTE*)(dwStartAddress + i), bMask, szMask) {
-      if(extract) {
-        return *(DWORD*)(dwStartAddress + i + codeOffset);
-      }
-      else {
-        return (DWORD)(dwStartAddress + i + codeOffset);
-      }
-    }
-  }
-       return false;
 }
